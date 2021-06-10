018cfde5ce1a5e611e41cedb636b8a0d1cfeca9a
Aleksandr <meat9@yandex.ru>
2021-06-10 16:56:36 +0300

    done

--- api/urls.py
+++ api/urls.py
@@ -2,18 +2,10 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-  #path('welcome/', views.welcome),
   path('create_auth/', views.create_auth), #Создание юзера
   path('get_all_users/', views.get_all_users), #список юзеров
   path('get_user/', views.get_user),# получить юзера
   path('put_user/<int:id>', views.put_user),
   path('patch_user/<int:id>', views.patch_user),
   path('delete_user/<int:id>', views.delete_user),
-
-
-#   path('addbook/', views.add_book),
-#   path('updatebook/<int:book_id>', views.update_book),
-#   path('deletebook/<int:book_id>', views.delete_book),
-#   path('register/', views.create_auth),
-
 ]
